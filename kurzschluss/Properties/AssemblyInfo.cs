1046fc031a5b30ceb630d4e57127641e4a0c138c
Leonid Lunin <leonid.lunin.main@gmail.com>
2022-08-10 21:57:12 -0400

    init commit

---
+++ kurzschluss/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyTrademark("FC")]
+[assembly: AssemblyTitle("kurzschlussreset v2")]
+[assembly: AssemblyProduct("kurzschlussreset")]
+[assembly: AssemblyCopyright("©  2014 FC")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4caff7de-25a7-4841-8f22-a0dceb19e566")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.1")]
